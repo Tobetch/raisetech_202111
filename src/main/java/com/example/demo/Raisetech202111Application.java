503b52dfbb71b4801f66d86a64b0c91d6fd99b1b
Tobetch <takutobe@gmail.com>
2021-12-25 14:36:06 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/Raisetech202111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raisetech202111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Raisetech202111Application.class, args);
+	}
+
+}
